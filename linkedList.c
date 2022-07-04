e38e6d2f3fc098f3bbf92dbc614468a8623a09e3
vishwajith barad <108567722+vishwajithBarad1@users.noreply.github.com>
2022-07-04 10:38:03 +0530

    Update linkedList.c

--- linkedList.c
+++ linkedList.c
@@ -1,4 +1,5 @@
 	#include<stdio.h>
+	#include<stdlib.h>
 	struct Node{
 		int data;
 		struct Node *next;
@@ -11,6 +12,26 @@
 		n->data=val;
 		return n;
 	}
+	void display(){
+		struct Node *temp=head;
+		while(temp!=NULL){
+			printf("%d->",temp->data);
+			temp=temp->next;
+		}
+		printf("\n");
+	}
+	void addAtB(int val){
+		struct Node *currNode;
+		currNode=createNode(val);
+		if(head!=NULL){
+			currNode->next=head;
+			head=currNode;
+		}
+		else{
+			head=currNode;
+		}
+		
+	}
 	void main(){
 		struct Node *n1,*n2,*n3;
 		n1=createNode(10);
@@ -19,4 +40,10 @@
 		head=n1;
 		n1->next=n2;
 		n2->next=n3;
+		display();
+		printf("this is a linked list before insertion\n");
+		addAtB(40);
+		addAtB(50);
+		addAtB(60);
+		display();
 	}
