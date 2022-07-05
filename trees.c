55d92a6af8c974aefba2894f82f1225f4f3a255d
vishwajith barad <108567722+vishwajithBarad1@users.noreply.github.com>
2022-07-05 10:17:15 +0530

    Update trees.c

--- trees.c
+++ trees.c
@@ -2,46 +2,19 @@
 	#include<stdlib.h>
 	struct Node{
 		int data;
-		struct Node *next,*prev;
+		struct Node *left,*right;
 	};
-	struct Node *head=NULL,*tail=NULL;
+	struct Node *Root=NULL;
 	struct Node *createNode(int val){
 		struct Node *n;
 		n=(struct Node*)malloc(sizeof(struct Node));
-		n->next=NULL;
+		n->left=NULL;
 		n->data=val;
-		n->prev=NULL;
+		n->right=NULL;
 		return n;
 	}
-	void display(){
-		struct Node *temp=head;
-		while(temp!=NULL){
-			printf("%d->",temp->data);
-			temp=temp->next;
-		}
-		printf("\n");
-	}
-	void addAtB(int val){
-		struct Node *currNode;
-		currNode=createNode(val);
-		if(head!=NULL){
-			currNode->next=head;
-			head->prev=currNode;
-			head=currNode;
-		}
-		else{
-			head=currNode;
-		}		
-	}
 	void main(){
-		int val,n,i,pos;
-		scanf("%d",&n);
-		for(i=0;i<n;i++){
-			val;
-			scanf("%d",&val);
-			addAtB(val);
-		}
-		display();
+		
 	}
 	
 	
