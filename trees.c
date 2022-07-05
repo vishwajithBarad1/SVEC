5091b7be5f6bbb2284eecb64680204545a7fa666
vishwajith barad <108567722+vishwajithBarad1@users.noreply.github.com>
2022-07-05 11:18:45 +0530

    Update trees.c

--- trees.c
+++ trees.c
@@ -1,31 +1,56 @@
-	#include<stdio.h>
-	#include<stdlib.h>
-	struct Node{
-		int data;
-		struct Node *left,*right;
-	};
-	struct Node *Root=NULL;
-	struct Node *createNode(int val){
-		struct Node *n;
-		n=(struct Node*)malloc(sizeof(struct Node));
-		n->left=NULL;
-		n->data=val;
-		n->right=NULL;
-		return n;
-	}
-	void main(){
-		struct Node *n1,*n2,*n3,*n4,*n5;
-		n1=createNode(10);
-		n2=createNode(20);
-		n3=createNode(30);
-		n4=createNode(40);
-		n5=createNode(50);
-		Root=n1;
-		n1->left=n2;
-		n1->right=n3;
-		n2->left=n4;
-		n2->right=n5;
-	}
-	
-	
-	
+#include<stdio.h>
+#include<stdlib.h>
+struct Node{
+	int data;
+	struct Node *left,*right;
+};
+struct Node *Root=NULL;
+struct Node *createNode(int val){
+	struct Node *n;
+	n=(struct Node*)malloc(sizeof(struct Node));
+	n->left=NULL;
+	n->data=val;
+	n->right=NULL;
+	return n;
+}
+void inorder(struct Node *root){
+	if(root==NULL)
+		return;
+	inorder(root->left);
+	printf("%d,",root->data);
+	inorder(root->right);
+}
+void postorder(struct Node *root){
+	if(root==NULL)
+		return;
+	postorder(root->left);
+	postorder(root->right);
+	printf("%d,",root->data);
+}
+void preorder(struct Node *root){
+	if(root==NULL)
+		return;
+	printf("%d,",root->data);
+	preorder(root->left);
+	preorder(root->right);
+}
+
+void main(){
+	struct Node *n1,*n2,*n3,*n4,*n5;
+	n1=createNode(10);
+	n2=createNode(20);
+	n3=createNode(30);
+	n4=createNode(40);
+	n5=createNode(50);
+	Root=n1;
+	n1->left=n2;
+	n1->right=n3;
+	n2->left=n4;
+	n2->right=n5;
+	inorder(Root);
+	printf("\n");		
+	preorder(Root);
+	printf("\n");
+	postorder(Root);
+
+}
